034d052c36ce56cb592a7d31ae0e84a96dbad3e4
ethanabbott10 <ethanabbott10@byui.edu>
2025-04-26 17:50:54 -0600

    Update Program.cs
    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+    
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Your letter grade: {letter}");
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine($"Your letter grade: {letter}");
+            Console.WriteLine("We are sorry, you did not pass the class. Hope you can learn from your mistakes.");
+        }
+
+
     }
 }
