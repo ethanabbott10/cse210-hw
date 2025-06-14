f2cc527750ae684b57e3fd82f6fc9859678efeab
ethanabbott10 <ethanabbott10@byui.edu>
2025-06-13 19:52:56 -0600

    updated develop04

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+
+public class Activity
+{
+    private string _startingMessage = "";
+    private int _duration = 0;
+    private string _endingMessage = "";
+
+    public Activity(int input)
+    {
+        if (input == 1)
+        {
+            
+        }
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int _index = 0;
+
+        do
+
+    }
+
+    public static int DisplayMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to the Mindfulness Program.");
+        Console.WriteLine("Please select one of the following activities:");
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Reflection Activity");
+        Console.WriteLine("3. Listing Activity");
+        Console.WriteLine("4. Quit");
+        Console.Write("Selection: ");
+        int _index = int.Parse(Console.ReadLine()) - 1;
     }
 }
