6c46c235c09da08d7e4dd0656d72b2426f12f868
ethanabbott10 <ethanabbott10@byui.edu>
2025-06-18 20:12:41 -0600

    Completed Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> _shapes = new List<Shape>();
+        Square square = new Square("burgundy", 10);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+        Rectangle rectangle = new Rectangle("burgundy", 10, 10);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+        Circle circle = new Circle("burgundy", 10);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+        _shapes.Add(square);
+        _shapes.Add(rectangle);
+        _shapes.Add(circle);
+        foreach (Shape shape in _shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
